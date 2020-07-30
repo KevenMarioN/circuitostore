37ad3e69b4625da870ce25b7646ec026f5b77573
Keven Mário <kevenmario.n.r@gmail.com>
2020-07-29 22:58:09 -0300

    Add function in router clients

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const router = require('./router');
+const router = require('./src/router');
 const morgan = require('morgan');
-const port = process.env.PORT || 3333;
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+
 app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(router);
--- backend/src/controllers/clientsController.js
+++ backend/src/controllers/clientsController.js
@@ -1,49 +1,93 @@
+const mysql = require ('../database/mysql').pool;
+const yup = require('yup');
+
+function dateToEN(date)
+{	
+	return date.split('/').reverse().join('-');
+}
+
+const clientsValidate = yup.object().shape({
+    name : yup.string().min(10).max(100).required(),
+    nameMother: yup.string().min(10).max(100).required(),
+    cpf : yup.string().min(14).max(15).required(),
+    rg : yup.string().min(14).max(15).required(),
+    numberPhone :yup.string().min(14).max(20).required(),
+    address : yup.string().min(10).max(100).required(),
+    number : yup.number().required(),
+    district : yup.string().min(10).max(50).required(),
+    year : yup.date().required(),
+    obs : yup.string(),
+    
+
+
+}) 
+
 class clientsController
 {
     async show(req,res)
     {
-        res.send({
-            clients : 
-            [
-            {
-            id : 0,
-            name : "XXXXX XXXXX",
-            nameMother : "XXX XXXXX",
-            cpf : "xxx-xxx-xxx.xxx",
-            rg : "xxx.xxx.xxx-xx",
-            address : "Street XXXXX XXXXX",
-            number : 0,
-            district : "XXXX XXXX",
-            year : "XX/XX/XXXX",
-            number : "XXXXXXXXXXX",
-            obs : "xxxxxxxxxxxxxx"
-            }
-            ]   
-        })
+       mysql.getConnection((error,conn) =>{
+           if(error) { return res.status(500).send({error : error})}
+            conn.query(
+                'SELECT * FROM clients',
+                (error,resultado,field) => {
+                    conn.release();
+
+                    if(error) {  return res.status(500).send({error : error,response:null})}
+
+                    res.status(200).send({clients : resultado});
+                }
+            )
+       });
     }
 
     async create(req,res)
     {
         const 
         {
-            
             name,
             nameMother,
             cpf,
             rg,
+            numberPhone,
             address,
             number,
             district,
             year,
             obs 
-        } = req.body; 
+        } = req.body;
+        //year = year.split("-").reverse().join("-");
+        
+        try
+        {
+            await clientsValidate.validate(req.body,{abortEarly :false})
+            mysql.getConnection((error,conn) =>{
+                if(error) { return res.status(500).send({error : error})}
+                
+                 conn.query(
+                     'INSERT INTO clients (name, nameMother, cpf, rg, address, `number`, district, yearbrith, obs, numberPhone) VALUES (?,?,?,?,?,?,?,?,?,?)',
+                     [name,nameMother,cpf,rg,address,number,district, year.split("-").reverse().join("-"), obs,numberPhone],
+                     (error,resultado,field) => {
+                         conn.release();
+     
+                         if(error) {  return res.status(500).send({error : error,response:null})}
+     
+                         res.status(200).send({id : resultado.insertId,messagem :"Cliente criado com sucesso!"});
+                     }
+                 )
+            });
+        } catch (error) {
+            return res.status(400).send({error:error})
+        }
+        
     }
 
     async update(req,res){
         res.send(
-            {messagem : "Client update"}
+            {messagem : process.env.MYSQL_USER}
         )
     }
+    
     async delete(req,res){
         res.send(
             {messagem : "Client deleted"}
